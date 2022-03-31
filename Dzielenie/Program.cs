445f9fa21b004f6531fed2b5153c381a12dd168d
Alicja <alichr07@gmail.com>
2022-03-31 20:56:14 +0200

    Dodaj pliki projektów.

---
+++ Dzielenie/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Dzielenie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sprawdzanie, czy druga liczba jest dzielnikiem pierwszej
+
+            Console.WriteLine("Podaj liczbę całkowitą: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj kolejną liczbę całkowitą: ");
+            int b = int.Parse(Console.ReadLine());
+            if (a % b == 0)
+            {
+                Console.WriteLine("Liczba {1} jest dzielnikiem liczby {0}", a, b);
+            }
+            else
+            {
+                Console.WriteLine("Liczba {1} nie jest dzielnikiem liczby {0}", a, b);
+            }
+            Console.ReadKey();
+        }
+    }
+}
